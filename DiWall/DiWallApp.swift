1185196e7e8dfa91ae2a11dafac0ce92c253e33d
Laura Belén Yachelini <lbyachelini@gmail.com>
2024-05-18 06:35:51 -0300

    Initial Commit

---
+++ DiWall/DiWallApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiWallApp.swift
+//  DiWall
+//
+//  Created by Laura Belen Yachelini on 18/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiWallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
